1c1005aaf2994bb4f948ea046e9bf5a38f8c7a1b
mfathyldz <fathyldz@outlook.com>
2026-07-18 00:14:45 -0400

    Library class createed

--- src/practices/Main.java
+++ src/practices/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Books book1 = new Books("The Alchemist", "Paulo Coelho", 15.99);
+        Book book1 = new Book("The Alchemist", "Paulo Coelho", 15.99);
 
-        Books book2 = new Books("Atomic Habits", "James Clear", 18.5);
+        Book book2 = new Book("Atomic Habits", "James Clear", 18.5);
 
-        Books book3 = new Books("Clean Code", "Robert C. Martin", 32.0);
+        Book book3 = new Book("Clean Code", "Robert C. Martin", 32.0);
 
         System.out.println(book1);
         System.out.println(book2);
@@ -19,17 +19,17 @@ public class Main {
         //book3.setPrice(-28.5);
 
 
-        ArrayList<Books> books = new ArrayList<>();
+        ArrayList<Book> books = new ArrayList<>();
         books.add(book1);
         books.add(book2);
         books.add(book3);
-        books.add(new Books("Java", "Kuzzat Altay", 25.86));
+        books.add(new Book("Java", "Kuzzat Altay", 25.86));
 
         System.out.println(books);
 
         System.out.println("-------------------------------------------------");
 
-        for (Books eachBook : books) {
+        for (Book eachBook : books) {
             System.out.println(eachBook.getTitle() + " : $" + eachBook.getPrice());
         }
 
