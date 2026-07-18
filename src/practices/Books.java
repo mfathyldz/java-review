d0f5f228630ac95324b6b8c4a492aef7d35ac1f7
mfathyldz <fathyldz@outlook.com>
2026-07-17 22:27:20 -0400

    Books Class Created

--- src/practices/Books.java
+++ src/practices/Books.java
@@ -1,12 +1,12 @@
 package practices;
 
-public class Book {
+public class Books {
 
     public String title;
     public String author;
     public double price;
 
-    public Book(String title, String author, double price) {
+    public Books(String title, String author, double price) {
         this.title = title;
         this.author = author;
         this.price = price;
--- src/practices/Main.java
+++ src/practices/Main.java
@@ -6,21 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Book book1 = new Book("The Alchemist", "Paulo Coelho", 15.99);
+        Books book1 = new Books("The Alchemist", "Paulo Coelho", 15.99);
 
-        Book book2 = new Book("Atomic Habits", "James Clear", 18.5);
+        Books book2 = new Books("Atomic Habits", "James Clear", 18.5);
 
-        Book book3 = new Book("Clean Code", "Robert C. Martin", 32.0);
+        Books book3 = new Books("Clean Code", "Robert C. Martin", 32.0);
 
         System.out.println(book1);
         System.out.println(book2);
         System.out.println(book3);
 
-        ArrayList<Book> books = new ArrayList<>();
+        ArrayList<Books> books = new ArrayList<>();
         books.add(book1);
         books.add(book2);
         books.add(book3);
-        books.add(new Book("Java", "Kuzzat Altay", 25.86));
+        books.add(new Books("Java", "Kuzzat Altay", 25.86));
 
         System.out.println(books);
 
